d90182db6c379f887a1b5c6791cf74459b050cc3
Yasyf Mohamedali <yasyf@yasyf.com>
2012-11-08 16:50:46 -0800

    added PDF and php bitly

--- absorbance_concentration_lab/index.php
+++ absorbance_concentration_lab/index.php
@@ -8,17 +8,30 @@ $cachefile .= '_'.base64_encode($_SERVER['QUERY_STRING']);
 
 if (file_exists($cachefile)) {
 
-include($cachefile);
+if($_REQUEST['print'] == "yes")
+	{
+			require_once("../PDF/mpdf.php");
+			$mpdf=new mPDF();
+			$mpdf->WriteHTML(file_get_contents($cachefile));
+			$mpdf->Output();
+			exit();
 
-exit;
+	}
+	else {
+		include($cachefile);
+		exit();
+	}
+
 
 }
 ob_start(); // start the output buffer
 // Your normal PHP script and HTML content here
+$title = "Analyzing Quantitative Relationships Involving Concentrations of Reactants and Products at Equilibrium";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+<title><?php echo $title; ?></title>
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="../style.css" />
 	</head>
@@ -140,7 +153,7 @@ ob_start(); // start the output buffer
 									    <tr>
 										<th>Tube</th>
 										<th>K<sub>eq</sub> A</th>
-										<th>K<sub>eq</sub> B*</th>
+										<th>K<sub>eq</sub> B<a href="#note">*</a></th>
 										<th>K<sub>eq</sub> C</th>
 										<th>K<sub>eq</sub> D</th>
 										</tr>			<?php
@@ -165,17 +178,25 @@ ob_start(); // start the output buffer
 						echo("</tr>");
 						}
 						echo "<tr><td></td></tr>";
-						$Aratio = $A[1]/$A[$n-1];
-						$Bratio = $B[1]/$B[$n-1];
-						$Cratio = $C[1]/$C[$n-1];
-						$Dratio = $D[1]/$D[$n-1];
+						$ratio = array();
+						$ratio['A'] = ratio($A);
+						$ratio['B'] = ratio($B);
+						$ratio['C'] = ratio($C);
+						$ratio['D'] = ratio($D);
 						echo("<tr id='trial totals'>");
-						echo "<td id='total tube'>Ratio</td>";
-						echo "<td id='total A'>$Aratio</td>";
-						echo "<td id='total B'>$Bratio</td>";
-						echo "<td id='total C'>$Cratio</td>";
-						echo "<td id='total D'>$Dratio</td>";
+						echo "<td id='total tube'>Deviation Ratio</td>";
+						echo "<td id='total A'>".$ratio["A"]."</td>";
+						echo "<td id='total B'>".$ratio["B"]."</td>";
+						echo "<td id='total C'>".$ratio["C"]."</td>";
+						echo "<td id='total D'>".$ratio["D"]."</td>";
 						echo("</tr>");
+						
+						$ratioAbs = array();
+						foreach ($ratio as $key => $value) {
+							$value = abs($value - 1);
+							$ratioAbs["$key"] = $value;
+						}
+						$validKeq = array_search(min($ratioAbs),$ratioAbs);
 						}
 	function dilute($Ci,$Lf,$Ld) {
 				$moles = $Ci*$Lf;
@@ -190,49 +211,73 @@ ob_start(); // start the output buffer
 			$CeqReact = $CiReact - $CeqProd;
 			return $CeqReact;
 	}
+	function ratio($array) {
 
+		$mean = $sum / count($array);
+		$ratio = max($array) / min($array);
+		return $ratio;
+	}
+	/* returns the shortened url */
+	function get_bitly_short_url($url,$login,$appkey,$format='txt') {
+	  $connectURL = 'http://api.bit.ly/v3/shorten?login='.$login.'&apiKey='.$appkey.'&uri='.urlencode($url).'&format='.$format;
+	  return curl_get_result($connectURL);
+	}
+
+	/* returns a result form url */
+	function curl_get_result($url) {
+	  $ch = curl_init();
+	  $timeout = 5;
+	  curl_setopt($ch,CURLOPT_URL,$url);
+	  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	  $data = curl_exec($ch);
+	  curl_close($ch);
+	  return $data;
+	}
+
+	$Url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	$PDFUrl = $Url."&print=yes";
+	$short_url = get_bitly_short_url($Url,'yasyf','R_a551579385b3fb84f6796876d396659b');
 	?>
 	  </tbody>
 	</table>
+	<a id="note"></a>
+	<span style="font-size:12px">*Previously known to be the actual calculation for K<sub>eq</sub></span>
+	<br />
+	<p><?php echo "Result: K<sub>eq</sub> Trial <strong>$validKeq</strong> is the correct equilibrium expression calculation."; ?><p>
 	</center>
-	<br /><br /><br /><br /><span id="short"></span>
+	<div style="font-size:16px">
+	<p style="font-size:16px"><a style="font-size:16px" href="<?php echo $PDFUrl ?>">Generate Downloadable PDF</a></p>
+	<p style="font-size:16px"><a style="font-size:16px" href="#" onclick="window.print();return false;">Print This Page</a></p>
+	<p id="short" style="font-size:16px">Link to this page: <a style='font-size:16px' href="<?php echo $short_url; ?>"><?php echo $short_url; ?></a></p>
 	</div>
-	<script type="text/javascript">
-	function showHide(ID) {
-			elem = document.getElementById(ID);
-			if (elem.style.display == 'none') {
-				elem.style.display = 'block';
-			}
-			else{
-				elem.style.display = 'none';
-			}
-		}
-		
-		 $.getJSON(
-		        "http://api.bitly.com/v3/shorten?callback=?", 
-		        { 
-		            "format": "json",
-		            "apiKey": "R_a551579385b3fb84f6796876d396659b",
-		            "login": "yasyf",
-		            "longUrl": window.location.href
-		        },
-		        function(response)
-		        {
-		document.getElementById("short").innerHTML =  "Link to this page: <a href='"+response.data.url+"'>"+response.data.url+"</a>";
-		}
-		    );
-
-
-	</script>
+	</div>
 
 	</body>
 </html>
 
 <?php
-// BOTTOM of your script
-$fp = fopen($cachefile, 'w'); // open the cache file for writing
-fwrite($fp, ob_get_contents()); // save the contents of output buffer to the file
-fclose($fp); // close the file
-ob_end_flush(); // Send the output to the browser
+		$html = ob_get_contents();
+		$fp = fopen($cachefile, 'w'); // open the cache file for writing
+		fwrite($fp, $html); // save the contents of output buffer to the file
+		fclose($fp); // close the file
+		chmod($cachefile,0755);
+	if($_REQUEST['print'] == "yes")
+	{
+			ob_end_clean();
+			require_once("../PDF/mpdf.php");
+			$mpdf=new mPDF();
+			$mpdf->WriteHTML($html);
+			$mpdf->Output();
+			exit();
+	}
+	else {
+		// BOTTOM of your script
+		ob_end_flush(); // Send the output to the browser
+		exit();
+		}
+
+
+
 
 ?>
